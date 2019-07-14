631f7b2eebcc7a2e02a82a29de88c34049ffd7c5
Tom <tom@tom-linux.fritz.box>
2019-07-14 17:25:52 +0200

    removed: button.js, because it didn't work

--- button.js
+++
@@ -1,4 +0,0 @@
-javascript:
-	var script = document.createElement('script');
-	script.src = "https://raw.githubusercontent.com/toluschr/translate-this/master/translate-this.js";
-	document.getElementsByTagName("head")[0].appendChild(script);
